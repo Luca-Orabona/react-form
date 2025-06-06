7ec1c89a5bc7f9afadcc1f65709e344d5c4acdf0
Luca-Orabona <lucaorabona94@gmail.com>
2025-06-06 16:59:08 +0200

    feat: form added

--- src/components/ListaArticoli.jsx
+++ src/components/ListaArticoli.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
+
 const ListaArticoli = ({ Posts }) => {
+    const [newPosts, setNewPost] = useState(Posts);
+    const [articleName, setArticleName] = useState("");
+
+    const newPost = {
+        titolo: articleName
+    }
+
+    const gestioneSubmit = (event) => {
+        event.preventDefault();
+        setNewPost([...newPosts, newPost]);
+        setArticleName("");
+
+    }
+   
     return (
         <>
             <div className="container">
                 <h1 className="text-center">Lista articoli</h1>
+
+                <form onSubmit={gestioneSubmit} className="d-flex justify-content-between mb-3">
+
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={articleName}
+                        onChange={(event) => setArticleName(event.target.value)}
+                    />
+
+                    <button className="btn btn-primary">Submit</button>
+
+                </form>
+
                 <ul className="list-group">
-                    {Posts.map((curPost) => (
-                        <li className="list-group-item">{curPost.titolo}</li>
+                    {newPosts.map((curPost, index) => (
+                        <li key={index} className="list-group-item">{curPost.titolo}</li>
 
                     ))}
 
