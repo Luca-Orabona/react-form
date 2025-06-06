5f5b43b93a0c9b1d0d12c74ae31f4c4c52ffe0f3
Luca-Orabona <lucaorabona94@gmail.com>
2025-06-06 19:50:48 +0200

    feat: bonus delete article

--- src/components/ListaArticoli.jsx
+++ src/components/ListaArticoli.jsx
@@ -10,32 +10,49 @@ const ListaArticoli = ({ Posts }) => {
 
     const gestioneSubmit = (event) => {
         event.preventDefault();
-        setNewPost([...newPosts, newPost]);
-        setArticleName("");
-
+        if (articleName.trim().length > 0) {
+            setNewPost([...newPosts, newPost]);
+            setArticleName("");
+        } else {
+            alert("inserisci il titolo di un articolo")
+        }
     }
-   
+
+    const deleteArticle = (index) => {
+        const newArrayPost = newPosts.toSpliced(index, 1);
+        setNewPost(newArrayPost); 
+    }
+
     return (
         <>
             <div className="container">
                 <h1 className="text-center">Lista articoli</h1>
 
-                <form onSubmit={gestioneSubmit} className="d-flex justify-content-between mb-3">
+                <form onSubmit={gestioneSubmit} className="d-flex justify-content-between align-items-center mb-3 gap-3">
+                    <div className="flex-grow-1">
+                        <label htmlFor="article" className="form-label">Crea nuovo articolo</label>
+                        <input
+                            id="articole"
+                            type="text"
+                            className="form-control"
+                            value={articleName}
+                            onChange={(event) => setArticleName(event.target.value)}
+                        />
+                    </div>
 
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={articleName}
-                        onChange={(event) => setArticleName(event.target.value)}
-                    />
-
-                    <button className="btn btn-primary">Submit</button>
+                    <button className="btn btn-primary align-self-end">Submit</button>
 
                 </form>
 
                 <ul className="list-group">
                     {newPosts.map((curPost, index) => (
-                        <li key={index} className="list-group-item">{curPost.titolo}</li>
+
+                        <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                            {curPost.titolo}
+                            <button onClick={() =>{deleteArticle(index)}} className="border-0 bg-white"><i className="fa-solid fa-trash-can text-danger"></i></button>
+                        </li>
+
+
 
                     ))}
 
